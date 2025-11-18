09be93559bfdf229e23ee3fb20d6e15c98600ead
JohnCorby <williamforgames6@gmail.com>
2025-11-17 16:41:13 -0800

    init commit

---
+++ src/main/kotlin/com/johncorby/gravityGuild2/GravityGuild2.kt
@@ -0,0 +1,14 @@
+package com.johncorby.gravityGuild2
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class GravityGuild2 : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
