6dabe69a0e0fc0f273cb45bf016024a58dbd6a3a
JohnCorby <williamforgames6@gmail.com>
2026-03-13 21:58:40 -0700

    update paper

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.11-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
     compileOnly("org.battleplugins:arena:4.0.2")
